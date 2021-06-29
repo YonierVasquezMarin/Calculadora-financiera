d9726d1216b937aaf7ebf73f2179e6dda28ee6f2
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2021-06-28 22:15:55 -0500

    Estilo del menu y botones creado

---
+++ JS/funcionesInterfazGrafica.js
@@ -0,0 +1,8 @@
+let elMenuEstaDesplegado = false;
+/**
+ * Desplegar el menú creador el cual está permanece oculto,
+ * y este sólo aparece al llamar a esta función.
+ */
+function desplegarMenuCreador() {
+    elMenuEstaDesplegado = true;
+}
