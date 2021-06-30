e879076dd613af0b3001ed6809a329fc6116f51c
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2021-06-29 20:25:15 -0500

    Menu creador completado

--- JS/funcionesInterfazGrafica.js
+++ JS/funcionesInterfazGrafica.js
@@ -9,8 +9,26 @@ function desplegarMenuCreador() {
     if(!elMenuEstaDesplegado) {
         elMenuEstaDesplegado = true;
         const menuCreador = document.querySelector("#menu-creador")
-        menuCreador.style = "padding: 14px;margin-bottom: 28px;height: 256px;opacity: 100%;"
+        menuCreador.style = "margin-bottom: 28px;height: 324px;opacity: 100%;"
+        const contenedorMenuCreador = document.querySelector("#contenedor-menu-creador")
+        contenedorMenuCreador.style = "padding: 14px"
+        menuCreador.setAttribute("data-menu-desplegado", "true")
     }
+}
+
+/**
+ * Ocultar el menu donde se crean los registros.
+ */
+function ocultarMenuCreador() {
+    elMenuEstaDesplegado = false
+    const menuCreador = document.querySelector("#menu-creador")
+    menuCreador.style = "height: 324px;"
+    const contenedorMenuCreador = document.querySelector("#contenedor-menu-creador")
+    contenedorMenuCreador.style = ""
+    menuCreador.setAttribute("data-menu-desplegado", "false")
+    setTimeout(()=>{
+        menuCreador.style = ""
+    }, 650)
 }
 
 /**
@@ -18,6 +36,8 @@ function desplegarMenuCreador() {
  * en el menú creador. Si tal texto dice "Ingreso" se procede a
  * cambiarlo por "Egreso", de lo contrario, si dice "Egreso" se 
  * cambia a "Ingreso".
+ * @param {String} tipoMovimiento es el texto que va a aparecen 
+ * en el elemento afectado. Se puede recibir "Ingreso" o "Egreso".
  */
 function cambiarTipoMovimientoEnPantalla(tipoMovimiento) {
     const tipoMovientoElement = document.querySelector("#tipoNuevoMovimiento")
@@ -29,17 +49,64 @@ function cambiarTipoMovimientoEnPantalla(tipoMovimiento) {
 }
 
 /**
- * Si el mensaje de error 1 está oculto se procede a mostrarlo
- * en pantalla. De lo contrario si el mensaje está visble se
- * procede a ocultarlo.
+ * Extrae el texto que el usuario halla introducido en el campo 
+ * para el titulo del nuevo movimiento a crear. Dicho campo está 
+ * en el menú creador.
+ * @returns {String} el titulo del movimiento que el usuario halla 
+ * elegido.
  */
-function cambiarVisibilidadMsgError1() {
+function obtenerTituloMovimientoACrear() {
+    const tituloMovimiento = document.querySelector("#tituloMovimiento")
+    return tituloMovimiento.value
+}
+
+/**
+ * Extrae el texto que el usuario halla introducido en el campo 
+ * para el valor del nuevo movimiento a crear. Dicho campo está 
+ * en el menú creador.
+ * @returns {String} el valor del movimiento que el usuario halla 
+ * elegido.
+ */
+function obtenerValorMovimientoACrear() {
+    const valorMovimiento = document.querySelector("#valorMovimiento")
+    return valorMovimiento.value
+}
+
+/**
+ * Verifica que ambos campos del titulo y valor del movimiento no estén 
+ * vacíos.
+ * @param {String} tituloMovimiento el titulo del movimiento ingresado por 
+ * el usuario.
+ * @param {String} valorMovimiento el valor del movimiento ingresado por 
+ * el usuario.
+ * @returns {Boolean} retorna true si ambos campos del titulo y valor 
+ * contienen texto, o retorna false si uno o los dos campos están vacios.
+ */
+function losDatosDelMovimientoEstanFull(tituloMovimiento, valorMovimiento) {
+    if(tituloMovimiento=="" || valorMovimiento=="") {
+        return false
+    }
+    return true
+}
+
+/**
+ * Si se especifica en el parámetro "seMuestra" un true se 
+ * procede a mostrar el mensaje de error, y si es un false 
+ * se procede a ocultarlo.
+ * @param {Boolean} seMuestra si es true se muestra, y si 
+ * es false se oculta.
+ */
+function cambiarVisibilidadMsgError1(seMuestra) {
     const msgError1 = document.querySelector("#msgError1")
-    if(!elMsgError1EstaVisible) {
-        elMsgError1EstaVisible = true
+    if(seMuestra) {
         msgError1.style = "opacity: 100%"
     } else {
-        elMsgError1EstaVisible = false
         msgError1.style = "opacity: 0%"
     }
+
+    // if(!losDatosDelMovimientoEstanFull()) {
+    //     msgError1.style = "opacity: 100%"
+    // } else {
+    //     msgError1.style = "opacity: 0%"
+    // }
 }
--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,34 @@
+/* Cuando se hace click en alguno de los dos botones grandes para
+hacer un "Ingreso" o "Egreso" */
 document.querySelectorAll(".btnsGrandes").forEach((btn)=>{
     btn.addEventListener("click", ()=>{
         desplegarMenuCreador()
         cambiarTipoMovimientoEnPantalla(btn.getAttribute("data-tipo"))
     })
+})
+
+
+document.querySelectorAll(".entrada-menu-creador").forEach((input)=>{
+    input.addEventListener("click", ()=>{
+        cambiarVisibilidadMsgError1(false)
+    })
+})
+
+/* Cuando se hace click en el boton de "Añadir" para crear un 
+nuevo registro */
+document.querySelector("#btnAddRegister").addEventListener("click", ()=> {
+    let tituloMovimiento = obtenerTituloMovimientoACrear()
+    let valorMovimiento = obtenerValorMovimientoACrear()
+    if(losDatosDelMovimientoEstanFull(tituloMovimiento, valorMovimiento)) {
+        //Aquí se guarda en memoria el registro
+        //Aquí se muestra un nuevo registro en pantalla
+    } else {
+        cambiarVisibilidadMsgError1(true)
+    }
+})
+
+/* Cuando se hace click en el botón de la flecha hacía arriba, para 
+ocultar el menu creador */
+document.querySelector("#btnOcultarMenuCreador").addEventListener("click", ()=> {
+    ocultarMenuCreador()
 })
